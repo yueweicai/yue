b4314fb1122feb0e08cfad565f20ae10b241fded
yueweicai <weicai.yue@orilore.com>
2017-07-10 17:34:00 +0800

    first

---
+++ src/main/java/com/orilore/entitys/Product.java
@@ -0,0 +1,14 @@
+package com.orilore.entitys;
+
+public class Product {
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+}
---
+++ src/main/java/yue/DBUtil.java
@@ -0,0 +1,18 @@
+package yue;
+import java.io.IOException;
+import java.io.Reader;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+public class DBUtil {
+	
+	public static void main(String[] args) throws IOException {
+		Reader r = Resources.getResourceAsReader("Configuration.xml");
+		SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+		SqlSessionFactory factory = builder.build(r);
+		SqlSession session = factory.openSession();
+		session.selectList("com.orilore.dao.IProductDAO.select");
+	}
+
+}
